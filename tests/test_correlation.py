2ae8fe761f3ae5ec4cd44df123a0bfc44a0b7928
Stefanos Stavrianos <57586599+stefanstavrianos@users.noreply.github.com>
2023-12-28 23:55:11 +0200

    Add files via upload

--- tests/test_correlation.py
+++ tests/test_correlation.py
@@ -1,1 +1,13 @@
+import unittest
+from econometrics.correlation import correlation
+
+class TestCorrelationFunction(unittest.TestCase):
+    def test_basic_functionality(self):
+        df = # create a test dataframe
+        result = correlation(df)
+        expected = # what the result should be
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
 
